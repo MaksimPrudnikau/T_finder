d83d40f32bc114e5cd403b9257e39683dc3364bd
Channel BelarusGame <Mrfreedok@gmail.com>
2020-05-18 16:19:08 +0300

    Добавьте файлы проекта.

---
+++ Header.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <conio.h>
+#include <algorithm>
+
+using namespace std;
+
+struct Meas {
+	string name;
+	double m;
+};
